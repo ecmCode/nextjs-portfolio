7e1fbc71030d871533dddf0d2df3bca755c02b15
Lee <elcheveremochilero@gmail.com>
2023-04-27 10:44:17 -0500

    page layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import Navbar from '@/pages/components/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from './components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Navbar/>
-      <Component {...pageProps} />
-    </>
+      <Layout>
+        <Navbar/>
+        <Component {...pageProps} />
+      </Layout>
   )
 }
