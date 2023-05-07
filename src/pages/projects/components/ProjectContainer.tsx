3827ce699e2642263bf7bf1ed0a402cb4c83dc37
Lee <elcheveremochilero@gmail.com>
2023-05-06 20:42:24 -0500

    Revert "fix errors"
    This reverts commit 300375c1236eeb0f6189effff26b629c28e9928c.

--- src/pages/projects/components/ProjectContainer.tsx
+++ src/pages/projects/components/ProjectContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 const ProjectContainer = ({projects}: Props) => {
     return ( 
         <ul className="grid grid-cols-1 md:grid-cols-2 justify-center items-center place-items-center w-full h-auto gap-4 py-10">
-            {projects && projects.map((project, index) => (
+            {projects.map((project, index) => (
                 <ProjectBox
                     key={index}
                     name={project.name}
