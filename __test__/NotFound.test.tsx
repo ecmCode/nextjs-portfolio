d15d98c5202a45a1b25ec436e37ef9a04b2b8140
Lee <elcheveremochilero@gmail.com>
2023-05-06 22:03:21 -0500

    404 page testing

---
+++ __test__/NotFound.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import NotFound from '@/pages/404';
+import '@testing-library/jest-dom';
+ 
+describe('Home', () => {
+  it('renders the component ', () => {
+    const {container} = render(<NotFound/>);
+    expect(container).toBeInTheDocument();
+  });
+
+  it('renders every element ', () => {
+    render(<NotFound/>);
+ 
+    const heading = screen.getByRole('heading', {
+        name: /404/i,
+    });
+
+    const link = screen.getByRole('link',{
+        name: /Home/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+    expect(link).toBeInTheDocument();
+  });
+});
