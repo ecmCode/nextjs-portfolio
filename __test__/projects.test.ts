9991566cb3eb0a24a074d6fa27779c9f52a79b56
Lee <elcheveremochilero@gmail.com>
2023-05-09 09:40:36 -0500

    projects array test

---
+++ __test__/projects.test.ts
@@ -0,0 +1,26 @@
+import { projects } from "@/lib/projects";
+
+test('if project list exists with right length', () => {
+    expect(projects).not.toBeUndefined()
+    expect(projects).toBeInstanceOf(Array)
+    expect(projects).toHaveLength(2)
+})
+
+test('if each project has all needed properties', () => {
+    projects.forEach((project) => {
+        expect(project).toHaveProperty('name')
+        expect(project).toHaveProperty('desc')
+        expect(project).toHaveProperty('imageURL')
+        expect(project).toHaveProperty('codeURL')
+        expect(project).toHaveProperty('demoURL')
+    })
+
+})
+
+test('if all urls match their regex patterns', () => {
+    projects.forEach((project) => {
+        expect(project.codeURL).toMatch(/https:\/\/github.com\/[-a-zA-Z]*\/[-a-zA-Z]*/i)
+        expect(project.demoURL).toMatch(/https:\/\/[-a-zA-Z]*.github.io\/[-a-zA-Z]*/i)
+    })
+
+})
