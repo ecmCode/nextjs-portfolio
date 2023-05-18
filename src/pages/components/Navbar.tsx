6a2ee329c96aab51570732d9e08371a0dc40116f
Lee <elcheveremochilero@gmail.com>
2023-05-17 19:26:08 -0500

    Navbar highlight current page

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,23 +1,28 @@
 import Link from "next/link";
 import style from "./styles/Navbar.module.css"
+import { useRouter } from "next/router";
+
+const routes = [
+    {name: "Home", link: '/'},
+    {name: "About", link: '/about'},
+    {name: "Projects", link: '/projects'},
+    {name: "Blogs", link: '/blogs'},
+]
 
 const Navbar = () => {
-
+    const router = useRouter()
     return (
         <nav className="relative">
             <ul className={style.nav}>
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/about">About</Link>
-                </li>
-                <li>
-                    <Link href="/projects">Projects</Link>
-                </li>
-                <li>
-                    <Link href="/blogs">Blogs</Link>
-                </li>
+                {routes.map(route => (
+                    <li key={route.name.toLowerCase()}>
+                        <Link href={route.link}
+                            className={router.pathname === route.link ? 'text-cyan-400' : ''}
+                        >
+                            {route.name}
+                        </Link>
+                    </li>
+                ))}
             </ul>
             <div className={style.fade}></div>
         </nav>
