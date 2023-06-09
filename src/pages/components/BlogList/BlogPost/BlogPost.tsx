dec5d0a58e5c18a04c14b51fc7b467bfc2589db0
Lee <elcheveremochilero@gmail.com>
2023-06-08 20:39:47 -0500

    style

---
+++ src/pages/components/BlogList/BlogPost/BlogPost.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import style from "./BlogPost.module.css";
+
+const BlogPost = ({ id, title, body }: Blog) => {
+  return (
+    <div className={style.box}>
+      <h3 className={style.title}>{title}</h3>
+      <p className={style.body}>{body}</p>
+      <span className="text-right lg:text-center">
+        <Link href={`/blogs/${id}`}>More</Link>
+      </span>
+    </div>
+  );
+};
+
+export default BlogPost;
