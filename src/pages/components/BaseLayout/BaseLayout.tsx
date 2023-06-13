72dffb64746cd998dd2da39df834349333cf77d1
Lee Li KP <84004066+ecmCode@users.noreply.github.com>
2023-06-13 17:14:59 -0500

    Style (#9)

--- src/pages/components/BaseLayout/BaseLayout.tsx
+++ src/pages/components/BaseLayout/BaseLayout.tsx
@@ -1,11 +1,16 @@
 import Gradient from "./Gradient/Gradient";
 import style from "./BaseLayout.module.css";
+import {Inter} from "next/font/google"
+
+const inter = Inter({subsets: ["latin"] , weight: ["100","200","300","400","500","600","700","800","900"]})
 
 const BaseLayout = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className={style.layout}>
-      <Gradient />
-      {children}
+    <div className={inter.className}>
+      <div className={style.layout}>
+        <Gradient />
+        {children}
+      </div>
     </div>
   );
 };
