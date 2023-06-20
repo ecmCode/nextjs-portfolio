8228b912e9c045ff06f36c3e29e0e72450dc07e7
Lee <elcheveremochilero@gmail.com>
2023-06-19 20:02:02 -0500

    home page blog section styling

--- src/pages/components/BlogList/BlogPost/BlogPost.tsx
+++ src/pages/components/BlogList/BlogPost/BlogPost.tsx
@@ -6,8 +6,8 @@ const BlogPost = ({ id, title, body }: BlogType) => {
     <div className={style.box}>
       <h3 className={style.title}>{title}</h3>
       <p className={style.body}>{body}</p>
-      <span className="text-right lg:text-center">
-        <Link href={`/blogs/${id}`}>More</Link>
+      <span className="text-right md:text-center">
+        <Link href={`/blogs/${id}`} className={style.btn}>More</Link>
       </span>
     </div>
   );
