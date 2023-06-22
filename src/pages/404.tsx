a732b2616f38279fcddefec5ab7cfd8d0e02c963
Lee <elcheveremochilero@gmail.com>
2023-06-22 15:01:02 -0500

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,3 @@
-import { GetServerSideProps } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -12,10 +11,11 @@ const NotFound = () => {
 
   return (
     <main>
-      <h1>404</h1>
-      <p>Page not found</p>
+      <div className="flex gap-4 items-center justify-center select-none">
+        <h1 className="border-r-4 pr-4">404</h1>
+        <h2>Page Not Found.</h2>
+      </div>
       <Link href="/">Home</Link>
-      <br />
     </main>
   );
 };
