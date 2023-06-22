bcfbb2fc8596293c21bf5724ce89ed5c6adf245f
Lee <elcheveremochilero@gmail.com>
2023-06-22 15:06:21 -0500

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -6,19 +7,23 @@ const NotFound = () => {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('/404');
+    router.push("/404");
   }, [router]);
 
   return (
-    <main>
-      <div className="flex gap-4 items-center justify-center select-none">
-        <h1 className="border-r-4 pr-4">404</h1>
-        <h2>Page Not Found.</h2>
-      </div>
-      <Link href="/">Home</Link>
-    </main>
+    <>
+      <Head>
+        <title>Page Not Found</title>
+      </Head>
+      <main>
+        <div className="flex gap-4 items-center justify-center select-none">
+          <h1 className="border-r-4 pr-4">404</h1>
+          <h2>Page Not Found.</h2>
+        </div>
+        <Link href="/">Home</Link>
+      </main>
+    </>
   );
 };
-
 
 export default NotFound;
