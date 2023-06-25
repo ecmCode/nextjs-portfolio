dab32b2bcff45da00e7bc8fd417d3ac04190a490
Lee <elcheveremochilero@gmail.com>
2023-06-25 17:20:34 -0500

    responsive design

--- src/components/home/Aboutme.tsx
+++ src/components/home/Aboutme.tsx
@@ -8,10 +8,10 @@ const Aboutme = () => {
     <>
       <Title>About Me</Title>
       <Container variant="2/3">
-        <div className="px-6 lg:px-20 py-10 flex flex-col gap-6">
+        <div className="px-0 sm:px-10 lg:px-20 py-6 lg:py-10 flex flex-col gap-6">
           {aboutme &&
             aboutme.slice(0, 3).map((p, index) => <p key={index}>{p}</p>)}
-          <div className="text-center md:text-right my-4">
+          <div className="text-center my-4 flex flex-col items-stretch sm:items-end">
             <Link href="/about">Read More</Link>
           </div>
         </div>
