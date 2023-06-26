fcd69f4d2c1fc8beda6a3e975411c05a4730a1f0
Lee <elcheveremochilero@gmail.com>
2023-06-26 11:54:37 -0500

    Container styling

--- src/components/home/Container/Container.tsx
+++ src/components/home/Container/Container.tsx
@@ -9,12 +9,10 @@ const Container = ({ variant, children }: ContainerType) => {
       case "2/3":
         return "min-h-[66vh]";
       case "full":
-        return "h-full";
+        return "min-h-full";
       default:
-        /**
-         * Exhaustive check making sure all options are listed.
-         * It should throw a compiler error if an option is missing
-         */
+        // Exhaustive check making sure all options are listed.
+        // It should throw a compiler error if an option is missing.
         const _exhaustiveCheck: never = variant;
         return _exhaustiveCheck;
     }
@@ -22,8 +20,8 @@ const Container = ({ variant, children }: ContainerType) => {
 
   return (
     <div className="w-full">
-      <div className={classes(option(), " h-full flex items-start")}>
-        <div className="p-4 w-full">{children}</div>
+      <div className={classes(option(), "h-full flex")}>
+        <div className="p-4 w-full h-full flex flex-col self-center">{children}</div>
       </div>
     </div>
   );
