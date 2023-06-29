085ed06ed4688a3f6d49decea41149d3d0929c2d
Lee <elcheveremochilero@gmail.com>
2023-06-29 09:41:57 -0500

    BlogPostSkeleton

--- src/types/BlogPostSkeleton.ts
+++ src/types/BlogPostSkeleton.ts
@@ -1,6 +1,33 @@
+import {  EntryFieldTypes } from "contentful";
 import { PostType } from "./PostType";
 
 export type BlogPostSkeleton = {
   contentTypeId: "blogPost";
-  fields: PostType;
+  fields: {
+    title: EntryFieldTypes.Text;
+    slug: EntryFieldTypes.Text;
+    tags: EntryFieldTypes.Text[];
+    content: EntryFieldTypes.RichText;
+    author: {
+        fields: {
+            name: EntryFieldTypes.Text;
+            isAdmin: EntryFieldTypes.Boolean;
+            email: EntryFieldTypes.Text;
+        };
+    };
+    thumbnail: {
+        fields: {
+            file: {
+                details: {
+                    image: {
+                        width: EntryFieldTypes.Number;
+                        height: EntryFieldTypes.Number;
+                    };
+                };
+                url: EntryFieldTypes.Text;
+                title: EntryFieldTypes.Text;
+            };
+        };
+    };
+  };
 };
