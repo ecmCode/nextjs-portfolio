ef4aba8a23c29a360a06918c24378e80d0c8d293
Lee <elcheveremochilero@gmail.com>
2023-07-10 11:23:40 -0500

    Footer

--- src/components/home/Footer/Footer.tsx
+++ src/components/home/Footer/Footer.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div className="my-10">by @ecmCode 2023</div>;
+  return (
+    <div className="my-6">
+      © 2023 by{" "}
+      <Link
+        href="https://github.com/ecmCode"
+        className="p-0 bg-transparent hover:brightness-110 text-cyan-400 underline underline-offset-2"
+      >
+        @ecmCode
+      </Link>
+      . All rights reserved.
+    </div>
+  );
 };
 
 export default Footer;
