f2b4b7dac6e73afbe6985208829b2d28d986425c
Lee <elcheveremochilero@gmail.com>
2023-07-19 19:42:09 -0500

    styling for project page

--- src/components/projects/ProjectBox.tsx
+++ src/components/projects/ProjectBox.tsx
@@ -1,5 +1,5 @@
-// import Image from "next/image";
 import Link from "next/link";
+import style from "./ProjectBox.module.css"
 
 const ProjectBox = ({
   name,
@@ -9,22 +9,24 @@ const ProjectBox = ({
   demoURL,
 }: ProjectType) => {
   return (
-    <li className="flex flex-col gap-6 justify-between items-center w-full h-full p-4 bg-slate-200/40 dark:bg-slate-400/10 card">
-      <h2>{name}</h2>
-      <div className="p-10">
-        <p>{desc}</p>
-      </div>
-      <div className="flex flex-col md:flex-row gap-6 justify-center items-stretch md:items-center w-full">
-        <Link href={demoURL || "/"} target="_blank" className="btn btn-action">
-          Demo
-        </Link>
-        <Link
-          href={codeURL || "/"}
-          target="_blank"
-          className="btn btn-secondary"
-        >
-          Code
-        </Link>
+    <li className="card card-project">
+      <div className={style.box}>
+        <h2>{name}</h2>
+        <div className="p-10">
+          <p>{desc}</p>
+        </div>
+        <div className={style.btns}>
+          <Link href={demoURL || "/"} target="_blank" className="btn btn-action">
+            Demo
+          </Link>
+          <Link
+            href={codeURL || "/"}
+            target="_blank"
+            className="btn btn-secondary"
+          >
+            Code
+          </Link>
+        </div>
       </div>
     </li>
   );
--- src/components/projects/ProjectContainer.tsx
+++ src/components/projects/ProjectContainer.tsx
@@ -2,7 +2,7 @@ import ProjectBox from "./ProjectBox";
 
 const ProjectContainer = ({ projects }: { projects: ProjectType[] }) => {
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 justify-center items-center place-items-center w-full h-auto gap-10 p-10">
+    <ul className="grid grid-cols-1 sm:grid-cols-2 justify-center items-center place-items-center w-full lg:w-4/5 h-auto gap-10 lg:gap-20 p-10">
       {projects?.map((project) => (
         <ProjectBox key={project.name.split(" ").join("-")} {...project} />
       ))}
