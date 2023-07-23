3c4bb9c9b44305cbc69fa79509c707c68968abf6
Lee <elcheveremochilero@gmail.com>
2023-07-22 20:12:52 -0500

    add /tags and [tag] pages routing

--- src/components/blogs/BlogPageContent.tsx
+++ src/components/blogs/BlogPageContent.tsx
@@ -1,16 +1,30 @@
 import { PostType } from "@/types/PostType";
 import BlogPost from "./BlogPost";
+import Link from "next/link";
 
 const BlogPageContent = ({
   posts,
   error,
+  title,
 }: {
   posts: PostType[];
   error: string;
+  title?: string;
 }) => {
   return (
     <main>
-      <h1>Blogs</h1>
+      <h1>{title || "All Blogs"}</h1>
+      <div id="options" className="flex gap-10 justify-center items-center ">
+        {title && (
+          <Link href="/blogs" className="btn btn-action">
+            All Blogs
+          </Link>
+        )}
+
+        <Link href="/blogs/tags" className="btn btn-secondary">
+          Tags
+        </Link>
+      </div>
       <ul className="grid grid-cols-1 lg:grid-cols-2 w-full sm:w-2/3 gap-10 my-10">
         {error ? (
           <p>Error: {error}</p>
---
+++ src/pages/blogs/tags/index.tsx
@@ -0,0 +1,44 @@
+import { client } from "@/client";
+import { BlogPostSkeleton } from "@/types/BlogPostSkeleton";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await client.getEntries<BlogPostSkeleton>({
+    content_type: "blogPost",
+  });
+
+  const tags = res.items
+    .map((item) => item.fields.tags)
+    .filter((tag) => !!tag)
+    .join()
+    .split(",");
+  return {
+    props: {
+      tags,
+    },
+  };
+};
+
+const AllTagsPage = ({ tags }: { tags: string[] }) => {
+  return (
+    <>
+      <Head>
+        <title>Tags</title>
+      </Head>
+      <main>
+        <h1>All {tags.length} Tags:</h1>
+        <ul className="flex items-center justify-center gap-2 w-1/3 flex-wrap ">
+          {tags.map((tag) => (
+            <li key={tag} className="btn btn-secondary">
+              <Link href={`/blogs/tags/${tag}`}>{tag}</Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+};
+
+export default AllTagsPage;
