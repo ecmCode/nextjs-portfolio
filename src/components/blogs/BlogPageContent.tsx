ecd0636f4397eb2d2e794fcd4730eb0a134e8ca7
Lee <elcheveremochilero@gmail.com>
2023-07-24 16:55:54 -0500

    responsive design for blog page

--- src/components/blogs/BlogPageContent.tsx
+++ src/components/blogs/BlogPageContent.tsx
@@ -20,12 +20,13 @@ const BlogPageContent = ({
             All Blogs
           </Link>
         )}
-
+      <div id="option-bar" className="my-6 sm:my-0">
         <Link href="/blogs/tags" className="btn btn-secondary">
           Tags
         </Link>
       </div>
-      <ul className="grid grid-cols-1 lg:grid-cols-2 w-full sm:w-2/3 gap-10 my-10">
+      </div>
+      <ul className="grid grid-cols-1 md:grid-cols-2 w-full lg:w-2/3 gap-10 p-0 sm:p-10">
         {error ? (
           <p>Error: {error}</p>
         ) : (
