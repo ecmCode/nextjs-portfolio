db91049180c7cf76cf0109ab15a4320f39ebd6ee
ecmcode <ecmcode.dev@gmail.com>
2023-07-27 19:19:13 -0500

    navbar behavior

--- src/components/global/NavBar/Navbar.tsx
+++ src/components/global/NavBar/Navbar.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
-// import style from "./Navbar.module.css";
 import { useRouter } from "next/router";
 import { SetStateAction } from "react";
 import { BsSun, BsMoonStars } from "react-icons/bs";
 const routes = [
-  { name: "Home", link: "/" },
+  // { name: "Home", link: "/" },
   { name: "About", link: "/about" },
   { name: "Projects", link: "/projects" },
   { name: "Blogs", link: "/blogs" },
@@ -22,9 +21,22 @@ const Navbar = ({
     <nav className="relative">
       <ul
         className="fixed w-full flex gap-10 justify-center items-center 
-  py-4 px-8 backdrop-blur-md z-50 
-  bg-slate-300/20 dark:bg-slate-700/30 ring-1 ring-slate-800/50 dark:ring-slate-200/50;"
+        py-4 px-8 backdrop-blur-md z-50 
+        bg-slate-300/20 dark:bg-slate-700/20 
+        ring-1 ring-slate-800/50 dark:ring-slate-200/50"
       >
+        <li className="text-lg md:text-xl">
+          <Link
+            href="/"
+            className={
+              router.pathname === "/"
+                ? "text-cyan-500 dark:text-cyan-500"
+                : "hover:text-cyan-500 dark:hover:text-cyan-500"
+            }
+          >
+            Home
+          </Link>
+        </li>
         {routes.map((route) => (
           <li
             key={route.name.toLowerCase()}
